934926f63b78e4cbc066f5a14b0247732034b5ed
Fatou C.S <102216255+Fat2CS@users.noreply.github.com>
2023-08-11 13:48:50 +0200

    Add files via upload
    new script for auto commit

---
+++ Fat2cs.Readme/readme.js
@@ -0,0 +1,48 @@
+module.exports = `# Fat2CS.
+
+
+<p align="center">
+ <img align="center" height= 350px width=85%  src="https://zupimages.net/up/23/32/rf26.gif"
+   alt="" /></a> 
+</p>
+
+    
+<h1 align="center">Hi 👋, I'm Fatou</h1>
+<h3 align="center">Challenged by the possibilities of digital marketing, and following my retraining, learning web development became obvious!
+  Whether for mobile applications or websites, I must continue to learn in order to become more efficient in my new job...with you perhaps?
+Front or Back I don't want to choose, Fullstack Developer I am!!! </h3>
+
+
+
+<p align="center">
+  <img align="center" width="40%" alt="GIF" src="https://media.giphy.com/media/zQhFEBrX6plKg/giphy.gif"/>
+</p>
+
+
+- 🔭 I’m currently working on [Defilady](https://defilady.netlify.app/)
+
+- 🌱 I’m currently learning **React js, React Native, Next.js, Python**
+
+- 👨‍💻 All of my projects are available at [https://github.com/Fat2CS?tab=repositories](https://github.com/Fat2CS?tab=repositories)
+
+- 📫 How to reach me **coulibalyfatoumata@hotmail.com**
+
+<h3 align="left">Connect with me:</h3>
+<p align="left">
+<a href="https://linkedin.com/in/https://www.linkedin.com/in/fatouseiller/" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="https://www.linkedin.com/in/fatouseiller/" height="30" width="40" /></a>
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> </a> <a href="https://heroku.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" alt="heroku" width="40" height="40"/> </a> <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40"/> </a> <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a> <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> <a href="https://reactnative.dev/" target="_blank" rel="noreferrer"> <img src="https://reactnative.dev/img/header_logo.svg" alt="reactnative" width="40" height="40"/> </a> <a href="https://zapier.com" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/zapier/zapier-icon.svg" alt="zapier" width="40" height="40"/> <img src="https://www.pngall.com/wp-content/uploads/2016/05/PHP-Logo-Free-Download-PNG.png" alt="lo"width="40" height="40"/> <img src="https://github.com/Fat2CS/Fat2CS/assets/102216255/2f05ab58-34ad-4664-9b39-e5bda6e62769" alt="wordpress" width="100" height="40"/> <img src="https://github.com/Fat2CS/Fat2CS/assets/102216255/728e9bd1-745f-4c10-b3e2-fe6466acdceb" alt="Python" width="100" height="40"/>
+</a> </p>
+
+
+
+Added the \`Fri 3 Aug 2023\`
+
+Last update on <#today_date>
+
+<#day_before_new_years> days before new years
+
+<#gabot_signing>
+`;
